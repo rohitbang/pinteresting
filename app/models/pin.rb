e46ddf9318439a972715cf1ff19cad4bcdce3728
Rohit Bang <rajrohitb7@gmail.com>
2014-07-03 16:45:08 +0530

    resolved issues regarding paperclip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,10 +1,7 @@
 class Pin < ActiveRecord::Base
-     belongs_to :user
-     has_attached_file :image, :styles  => { :large=> "600X600>", :medium => "400x400>", :thumb => "100x100>" }, :processors => [:cropper]
-     
-
-     validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
-     
+	belongs_to :user
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 	
 	validates :image, presence: true
 	validates :description, presence: true
